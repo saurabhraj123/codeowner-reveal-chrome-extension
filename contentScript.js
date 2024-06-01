d559a4048405de8d24fef626489ad4d0cd80a228
saurabhraj123 <me4saurabh4u@gmail.com>
2024-06-01 15:31:57 +0530

    filter dropdown is shown

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,6 @@
 let trie = null;
+let codeOwnersFileContent = "";
+let codeownersObjForUniqueness = [];
 
 class TrieNode {
   constructor() {
@@ -85,6 +87,7 @@ const addCodeownerToElement = () => {
     const fileName = fileContainer.getAttribute("data-file-path");
 
     const codeowner = getCodeowner(fileName);
+    codeownersObjForUniqueness[codeowner] = codeowner;
 
     const codeownerElement = document.createElement("div");
     codeownerElement.classList.add("codeowner");
@@ -93,13 +96,138 @@ const addCodeownerToElement = () => {
   }
 };
 
+const createFileFilterElement = () => {
+  const fileFilterElement = document.querySelector("file-filter");
+  const codeownerFilter = document.querySelector(".custom-codeowner-filter");
+  if (!fileFilterElement || codeownerFilter) return;
+
+  const filterElement = document.createElement("file-filter");
+  filterElement.setAttribute("data-target", "diff-file-filter.fileFilter");
+  filterElement.setAttribute(
+    "data-action",
+    "file-filter-change:diff-file-filter#applyFilter"
+  );
+  filterElement.setAttribute("data-catalyst", "");
+  filterElement.classList.add("custom-codeowner-filter");
+
+  const detailsElement = document.createElement("details");
+  detailsElement.classList.add(
+    "diffbar-item",
+    "details-reset",
+    "details-overlay"
+  );
+
+  const summaryElement = document.createElement("summary");
+  summaryElement.classList.add("Link--muted", "select-menu-button");
+  summaryElement.setAttribute("aria-haspopup", "menu");
+  summaryElement.setAttribute("data-target", "file-filter.summary");
+  summaryElement.setAttribute("role", "button");
+
+  const strongElement = document.createElement("strong");
+  strongElement.classList.add(
+    "js-file-filter-text",
+    "css-truncate",
+    "css-truncate-target"
+  );
+  strongElement.setAttribute("data-target", "file-filter.fileFilterActiveText");
+  strongElement.textContent = "Codeowners";
+
+  summaryElement.appendChild(strongElement);
+  detailsElement.appendChild(summaryElement);
+
+  const detailsMenuElement = document.createElement("details-menu");
+  detailsMenuElement.classList.add("SelectMenu", "js-file-filter");
+  detailsMenuElement.setAttribute("role", "menu");
+
+  const selectMenuModalElement = document.createElement("div");
+  selectMenuModalElement.classList.add("SelectMenu-modal");
+
+  const headerElement = document.createElement("header");
+  headerElement.classList.add("SelectMenu-header");
+
+  const h3Element = document.createElement("h3");
+  h3Element.classList.add("SelectMenu-title");
+  h3Element.textContent = "Filter by codeowner";
+
+  headerElement.appendChild(h3Element);
+  selectMenuModalElement.appendChild(headerElement);
+
+  const selectMenuListElement = document.createElement("div");
+  selectMenuListElement.classList.add(
+    "SelectMenu-list",
+    "SelectMenu-list--borderless"
+  );
+
+  const formElement = document.createElement("form");
+  formElement.classList.add("js-file-filter-form");
+  formElement.setAttribute("data-turbo", "false");
+  formElement.setAttribute("action", "/");
+  formElement.setAttribute("accept-charset", "UTF-8");
+  formElement.setAttribute("method", "post");
+
+  const authenticityTokenInputElement = document.createElement("input");
+  authenticityTokenInputElement.setAttribute("type", "hidden");
+  authenticityTokenInputElement.setAttribute("name", "authenticity_token");
+  authenticityTokenInputElement.setAttribute("value", "YOUR_AUTH_TOKEN"); // Replace with actual token
+  authenticityTokenInputElement.setAttribute("autocomplete", "off");
+  formElement.appendChild(authenticityTokenInputElement);
+
+  const fieldsetElement = document.createElement("fieldset");
+
+  const legendElement = document.createElement("legend");
+  legendElement.classList.add("sr-only");
+  legendElement.textContent = "Filter by codeowner";
+  fieldsetElement.appendChild(legendElement);
+
+  const codeownersArray = Object.keys(codeownersObjForUniqueness);
+  console.log({ codeownersObjForUniqueness, codeownersArray });
+  codeownersArray.forEach((codeowner) => {
+    if (codeowner) {
+      const labelElement = document.createElement("label");
+      labelElement.classList.add("SelectMenu-item");
+      labelElement.setAttribute("role", "menuitem");
+
+      const inputElement = document.createElement("input");
+      inputElement.classList.add("js-diff-file-type-option", "mr-2");
+      inputElement.setAttribute("type", "checkbox");
+      inputElement.setAttribute("value", codeowner);
+      inputElement.setAttribute("name", "file-filters[]");
+      console.log({ inputElement });
+
+      labelElement.appendChild(inputElement);
+      labelElement.append(` ${codeowner}`);
+      fieldsetElement.appendChild(labelElement);
+      inputElement.checked = true;
+    }
+  });
+
+  formElement.appendChild(fieldsetElement);
+  selectMenuListElement.appendChild(formElement);
+  selectMenuModalElement.appendChild(selectMenuListElement);
+  detailsMenuElement.appendChild(selectMenuModalElement);
+  detailsElement.appendChild(detailsMenuElement);
+  filterElement.appendChild(detailsElement);
+
+  // Add the created element to the DOM
+  const parentElement = fileFilterElement.parentElement; // Replace with actual parent selector
+  if (parentElement) {
+    parentElement.appendChild(filterElement);
+  }
+};
+
+const addFilter = () => {
+  createFileFilterElement();
+};
+
 window.onload = () => {
   getCodeownersFileContent()
     .then((codeownersFileContent) => {
+      codeOwnersFileContent = codeownersFileContent;
       if (codeownersFileContent) {
         trie = buildTrie(codeownersFileContent);
 
         addCodeownerToElement();
+        addFilter();
       }
     })
     .catch((error) => {
@@ -110,6 +238,7 @@ window.onload = () => {
     for (const entry of entries) {
       if (entry.addedNodes.length > 0) {
         addCodeownerToElement();
+        addFilter();
       }
     }
   });
